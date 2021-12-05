2d46687833c050e544fbc423047ebb4c7624944a
Chris Smith <chrisronsmith@gmail.com>
2021-12-05 14:09:42 -0500

    oweo

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello")
+import * as Tone from 'https://cdn.skypack.dev/tone';
+
+window.addEventListener('gamepadconnected', () => {
+  const s = new Tone.Synth();
+  s.toDestination()
+
+  let previousPressed = false
+
+  let loop = () => {
+    const gamePads = navigator.getGamepads();
+    for (const key in gamePads) {
+      const buttons = gamePads[key]?.buttons;
+      if (!buttons) continue ;
+      const pressed = buttons.reduce((acc, cur) => acc || cur.pressed, false);
+      if (pressed && !previousPressed) {
+        s.triggerAttackRelease("c4", "4n")
+      }
+      previousPressed = pressed
+    }
+
+    requestAnimationFrame(loop)
+  }
+  loop();
+})
